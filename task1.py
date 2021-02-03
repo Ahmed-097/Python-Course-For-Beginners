e26763be4318d5653c2c7aee669e5556bd04c947
danaelshrbiny10 <https://github.com/danaelshrbiny10/automated-price-comparison.git>
2021-02-03 02:04:33 +0200

    Questions of task1

---
+++ task1.py
@@ -0,0 +1,85 @@
+## Q1 :
+# What will the following code produce?
+a = 2
+a = 4
+a = 6
+print(a + a + a)
+
+# --------------------------------------------------------------------
+
+## Q2 :
+# What's wrong with the following script?
+a = 1
+_a = 2
+_a2 = 3
+2a = 4
+
+# ----------------------------------------------------------------------
+
+## Q3 :
+# Fix the last line so that it outputs the sum of 1 and 2
+a = "1"
+b = 2
+print(a + b)
+
+# Expected output: 3
+
+# -------------------------------------------------------------------------
+
+## Q4 :
+# Executing the code will throw an error. Can you explain why?
+a = 1
+b = 2
+print(a == b)
+print(b == c)
+
+# ----------------------------------------------------------------------------
+
+## Q5 :
+10 / 3 = ??
+10 // 3 = ??
+10 % 3 = ??
+# ----------------------------------------------------------------------------
+
+## Q6 :
+#  what's the output ?
+x = 20
+x +=20
+print(x)
+
+# -------------------------------------------------------------------------------
+## Q7 :
+# fix the error 
+name = 'welcome in python"
+
+# -------------------------------------------------------------------------------
+
+## Q8 :   'variables'
+
+# what's the different between high level language & low level language ?
+# python interpeted or compiled language ?
+# can you use ; in python ?   explain your answer if true or false
+# what's bool type ?
+# tell me the different between :
+#       1)  == and =
+#       2)  '' , "" and ''' '''
+#       3)  int , str and float
+#       4)   /  , //  and  %
+
+# what's meant by using !=
+# steps of Operator Precedenc
+# answer this : (20+2**3)*2-(6+3*4)
+# how to write comment in python
+
+# -----------------------------------------------------------------------------------
+
+## Q9 : true or false
+
+1) n = 20 , n2 = "20"  ====> n + n2 =40
+2) name = 'python' or "python"
+3) x = 'what's python'
+4) addres = "egypt,street ahmed"arabi"
+5) 20/5 = 4
+6) ?age = 20
+7) float ====> double in memory
+8) /t ==> new line   ,  /n ===> tab
