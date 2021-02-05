f5385319262557fab3e7acb752a93a0eb5c7231d
Ahmed-097 <78576711+Ahmed-097@users.noreply.github.com>
2021-02-05 04:45:52 +0200

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,114 @@
+## Q1 :
+# What will the following code produce?
+a = 2
+a = 4
+a = 6
+print(a + a + a)
+output=18
+
+# --------------------------------------------------------------------
+
+## Q2 :
+# What's wrong with the following script?
+a = 1
+_a = 2
+_a2 = 3
+2a = 4 #this is wrong 
+
+# ----------------------------------------------------------------------
+
+## Q3 :
+# Fix the last line so that it outputs the sum of 1 and 2
+a = "1"
+b = 2
+print(int(a)+ b)
+
+# Expected output: 3
+
+# -------------------------------------------------------------------------
+
+## Q4 :
+# Executing the code will throw an error. Can you explain why?
+a = 1
+b = 2
+print(a == b)
+print(b == c)
+
+#because c is not defined
+# ----------------------------------------------------------------------------
+
+## Q5 :
+10 / 3 = 3.333335
+10 // 3 = 3
+10 % 3 = 1
+# ----------------------------------------------------------------------------
+
+## Q6 :
+#  what's the output ?
+x = 20
+x +=20
+print(x)
+# the output 40
+# -------------------------------------------------------------------------------
+## Q7 :
+# fix the error 
+name = 'welcome in python"
+#name = "welcome in python"
+#name = 'welcome in python'
+
+# -------------------------------------------------------------------------------
+
+## Q8 :   'variables'
+
+# what's the different between high level language & low level language ?
+
+
+# high level language easy to learn       #low level not easy
+# near to human languages                 #far from human languages 
+#python                                   # c 
+
+# python interpeted or compiled language ?
+#interpeted
+
+# can you use ; in python ?   explain your answer if true or false
+#true but we just use it in separation
+
+
+# what's bool type ?
+# bool type (Logical operations and the output is either true or false)
+
+
+# tell me the different between :
+#       1)  ==  (must be exactly equal) and  =  (equal)
+
+#       2)  ''(نص) , ""(نص)and ''' '''(تعليق طويل او براجراف)
+
+#       3)  int(ارقام صحيحه) , str(نصوص) and float(ارقام عشرية)
+
+#       4)   /(قسمة)  , //(نتجاهل الباقي)  and  %(باقي القسمة)
+#_________________________________________________________________
+
+
+# what's meant by using !=          not equal
+
+# steps of Operator Precedence  1-العمليات داخل الاقواس
+                               #2-الاسس
+                               #3-الضرب والقسمة
+                               #4-الجمع والطرح
+
+# answer this : (20+2**3)*2-(6+3*4)  = 38
+
+# how to write comment in python?    by using(#)
+
+# -----------------------------------------------------------------------------------
+
+## Q9 : true or false
+
+1) n = 20 , n2 = "20"  ====> n + n2 =40 #false
+2) name = 'python' or "python"          #ture
+3) x = 'what's python'                  #false
+4) addres = "egypt,street ahmed"arabi"  #false    
+5) 20/5 = 4                             #true
+6) ?age = 20                            #true
+7) float ====> double in memory         #true
+8) /t ==> new line   ,  /n ===> tab     #false
